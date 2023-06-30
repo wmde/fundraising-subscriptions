2973495554abab31a003f6b930f18f1a52519b71
Abban Dunne <abban.dunne@wikimedia.de>
2023-06-30 13:12:43 +0200

    Remove Gedmo
    * Remove the Gedmo Doctrine Extensions and update configuration.
    * Make Subscriptions add a created date in their constructor.
    * Update PhpUnit configuration.

--- tests/TestSubscriptionContextFactory.php
+++ tests/TestSubscriptionContextFactory.php
@@ -4,18 +4,14 @@ declare( strict_types = 1 );
 
 namespace WMDE\Fundraising\SubscriptionContext\Tests;
 
-use Doctrine\Common\EventManager;
-use Doctrine\Common\EventSubscriber;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\Configuration;
 use Doctrine\ORM\EntityManager;
-use WMDE\Fundraising\SubscriptionContext\SubscriptionContextFactory;
 
 class TestSubscriptionContextFactory {
 
 	private Configuration $doctrineConfig;
-	private SubscriptionContextFactory $factory;
 	private Connection $connection;
 	private ?EntityManager $entityManager;
 
@@ -26,33 +22,17 @@ class TestSubscriptionContextFactory {
 			'driver' => 'pdo_sqlite',
 			'memory' => true,
 		] );
-		$this->factory = new SubscriptionContextFactory();
 		$this->entityManager = null;
 	}
 
 	public function getEntityManager(): EntityManager {
 		if ( $this->entityManager === null ) {
-			$eventManager = $this->setupEventSubscribers( $this->factory->newEventSubscribers() );
-
 			$this->entityManager = new EntityManager(
 				$this->connection,
-				$this->doctrineConfig,
-				$eventManager
+				$this->doctrineConfig
 			);
 		}
 
 		return $this->entityManager;
-	}
-
-	/**
-	 * @param EventSubscriber[] $eventSubscribers
-	 * @return EventManager
-	 */
-	private function setupEventSubscribers( array $eventSubscribers ): EventManager {
-		$eventManager = new EventManager();
-		foreach ( $eventSubscribers as $eventSubscriber ) {
-			$eventManager->addEventSubscriber( $eventSubscriber );
-		}
-		return $eventManager;
 	}
 }
